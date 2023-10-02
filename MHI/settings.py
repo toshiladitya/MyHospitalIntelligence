2845d6ef7c32dc8ae1d682daf095aa1fcc0ff004
Aditya Sharma <adityasharmanimod@gmail.com>
2023-10-02 14:30:18 +0530

    email errors

--- MHI/settings.py
+++ MHI/settings.py
@@ -158,8 +158,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='toshiladityasharma@gmail.com'
-EMAIL_HOST_PASSWORD ='nbzfstcjqzlqwzlr'
+EMAIL_HOST_USER ='toshiladityasharma1@gmail.com'
+EMAIL_HOST_PASSWORD ='vitdqtwmwziabdpo'
 
 
 # Simple_JWT settings
